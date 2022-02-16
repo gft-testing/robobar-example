27426ee824d96ee76b5dde0978d6f16bfaf23e2f
Javier Rodriguez <profesor@sinensia.com>
2022-02-16 13:05:32 +0100

    Added cucumber preprocessor.

--- cypress/integration/hola.spec.js
+++ cypress/integration/hola.spec.js
@@ -5,6 +5,9 @@
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
 
+function checkoutButton() {
+    return cy.get('.col-12 > .btn')
+}
 function colaButton() {
     return cy.get(':nth-child(1) > :nth-child(3) > .row > .col-5 > .input-group-append > .btn')
 }
@@ -14,22 +17,45 @@ function totalText() {
 }
 
 describe('Hello Cypress', () => {
+    beforeEach(()=>{
+        cy.visit('http://localhost:3002')
+    })
     it('Says hello', () => {
         expect(true).to.equal(true)
     })
     it('Order a cola', () => {
-        cy.visit('http://localhost:3002/')
         colaButton()
             .click()
         totalText()
             .should("contain.text", "€1.25")
     })
+    it('Order a cola', () => {
+        checkoutButton()
+            .should("be.disabled")
+        colaButton()
+            .click()
+        checkoutButton()
+            .should("be.enabled")
+        totalText()
+            .should("contain.text", "€1.25")
+    })
     it('Order two colas', () => {
-        cy.visit('http://localhost:3002/')
         colaButton()
             .click()
             .click()
         totalText()
             .should("contain.text", "€2.50")
+    })
+    it('Order two beers', () => {
+        cy.get(':nth-child(2) > :nth-child(3) > .row > .col-5 > .input-group-append > .btn')
+            .as('beerButton')
+            .click()
+        cy.get(':nth-child(4) > .ng-binding')
+            .as('total')
+            .should("contain.text", "€2.00")
+        cy.get('@beerButton')
+            .click()
+        cy.get('@total')
+            .should("contains.text","€4.00" )
     })
 })
